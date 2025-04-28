e4a2a2fa7842d9376b04c69aefdb940cb3cc74db
Jupiter Bold <sanadababy5@gmail.com>
2025-04-28 12:43:04 +0100

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'anthony.campbel246@outlook.com',
-    password: 'susanoo900#&112',
+    email: 'tanuki8171@gmail.com',
+    password: 'susanoo900#',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
